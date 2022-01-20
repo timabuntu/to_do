2ab3fb00bc835e2e24421c8cb544265ea1a36052
thiago lopes de mello <timabuntu@gmail.com>
2022-01-20 16:21:28 -0300

    :tada: clean boileplate

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="mx-auto h-screen">
+      <div className="bg-blue-800 h-1/4 text-white flex justify-center">
+        <h1>To.do</h1>
+      </div>
+    </div>
+  );
+}
