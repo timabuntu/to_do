ce5c2ad1878e37debe07da7c6e2bea25597a44d9
thiago lopes de mello <timabuntu@gmail.com>
2022-01-20 16:27:17 -0300

    feat: Header created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <div className="mx-auto h-screen">
+    <div className="mx-auto h-screen bg-slate-100">
       <div className="bg-blue-800 h-1/4 text-white flex justify-center">
-        <h1>To.do</h1>
+        <Header />
       </div>
     </div>
   );
